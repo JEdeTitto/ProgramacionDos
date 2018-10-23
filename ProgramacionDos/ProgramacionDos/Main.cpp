c1507c380d3ce658a1e44d80c68559d76cc6ab44
Juan Di Modugno <dimodugnojuan@gmail.com>
2018-10-22 21:40:23 -0300

    Actualizo leyenda

--- ProgramacionDos/ProgramacionDos/Main.cpp
+++ ProgramacionDos/ProgramacionDos/Main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-	Base __base = Base("Soy julian");
+	Base __base = Base("Soy Juan Di Modugno");
 	cout << __base.getFirma() << endl;
 	cout << "123123123" << endl;
 	_getch();
