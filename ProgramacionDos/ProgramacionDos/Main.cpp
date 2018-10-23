42a557194db21805076a3aa24f8a17700a32aa75
Juan Di Modugno <dimodugnojuan@gmail.com>
2018-10-22 21:22:59 -0300

    Update text of base signature

--- ProgramacionDos/ProgramacionDos/Main.cpp
+++ ProgramacionDos/ProgramacionDos/Main.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 
 int main() {
-	Base __base = Base("ProgII");
+	Base __base = Base("Nuevo texto actualizado");
 	cout << __base.getFirma() << endl;
+	cout << "123123123" << endl;
 	_getch();
 	return 0;
 }
