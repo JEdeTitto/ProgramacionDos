b5d6ec194c1cc5660ce00f9ff84a35c0f70619ce
DESKTOP-SEF1AD1\Julian <julianeloy.detitto@gmail.com>
2018-10-23 02:38:18 +0200

    agrego string

--- ProgramacionDos/ProgramacionDos/Main.cpp
+++ ProgramacionDos/ProgramacionDos/Main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-	Base __base = Base("Nuevo texto actualizado");
+	Base __base = Base("Soy julian");
 	cout << __base.getFirma() << endl;
 	cout << "123123123" << endl;
 	_getch();
